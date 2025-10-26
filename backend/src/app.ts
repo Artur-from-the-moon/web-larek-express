4c6af3b8b05bdd6646df48239ee4a7de4fad8724
Arthur-Adamov <artur.adamov03340@yandex.ru>
2025-10-26 21:50:04 +0400

    feat(backend): добавлены get и post роуты и добавлены картинки в папку public

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
+import router from './routes/products';
 
 const app = express();
 
+app.use(express.json());
+
 mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
 
-app.listen(3000, () => { console.log('listening on port 3000'); });
+app.use('/products', router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
